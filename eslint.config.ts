f9336eec506da0fd06148a29aa4debe63fa62565
Tony Rinaldi <tony@grazzy.com>
2025-06-13 14:40:49 -0500

    remove oxlint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,6 @@
 import { globalIgnores } from 'eslint/config';
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
 import pluginVue from 'eslint-plugin-vue';
-import pluginOxlint from 'eslint-plugin-oxlint';
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
@@ -17,10 +16,10 @@ export default defineConfigWithVueTs(
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
-  ...pluginOxlint.configs['flat/recommended'],
   skipFormatting,
   {
     rules: {
+      'no-console': 'warn',
       '@typescript-eslint/no-explicit-any': 'off',
       'vue/multi-word-component-names': 'off',
     },
